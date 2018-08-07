77bfa2bb458f72d4533cd88e494f1d89a1eaa252
Peopl3s <nikposd@mail.ru>
2018-08-07 17:13:15 +0300

    Fixed whitespaces

--- insta.py
+++ insta.py
@@ -10,9 +10,9 @@ import datetime
 #os.environ['NO_PROXY'] = 'instagram.com'
 
 form = cgi.FieldStorage() # парсинг данных формы
-l=threading.Lock()#создаём блокировку
+l = threading.Lock() #создаём блокировку
 
-'''def loadF(newurl,photoname):
+'''def loadF(newurl, photoname):
     try:
         urllib.request.urlretrieve(newurl, 'C://Server/www/'+photoname+'.jpg')
     except Exception as err:
@@ -22,32 +22,32 @@ def Uol():
     try:
         with l:
             link = str(urllib.request.urlopen(cgi.escape(form['link'].value)).read())
-            ex=re.compile(r'(("display_url"|"video_url"):"https://(.*?).(jpg|mp4)")')
-            result=ex.finditer(link)
-            photos=[]
+            ex = re.compile(r'(("display_url"|"video_url"):"https://(.*?).(jpg|mp4)")')
+            result = ex.finditer(link)
+            photos = []
             for ma in result:
                 if str(ma.group(0)) not in photos:
                     photos.append(str(ma.group(0)))
-            if len(photos)==0:
+            if len(photos) == 0:
                  print("<div align=\"center\">")
                  print("<h1>Wrong link</h1>")
                  print("</div>")
-            delsize=0
-            typecontent=""
+            delsize = 0
+            typecontent = ""
             for newurl in list(photos):
                 if "video" in newurl:
-                    delsize=13
-                    typecontent="<div id=\"cont\" align=\"center\"><video controls=\"controls\"><source src=\"$URL$\"></video></div>"
+                    delsize = 13
+                    typecontent = "<div id=\"cont\" align=\"center\"><video controls=\"controls\"><source src=\"$URL$\"></video></div>"
 	
                 else:
-                    delsize=15
-                    typecontent="<div id=\"cont\" align=\"center\"><img src=\"$URL$\" alt=\"альтернативный текст\"> </div>"
+                    delsize = 15
+                    typecontent = "<div id=\"cont\" align=\"center\"><img src=\"$URL$\" alt=\"альтернативный текст\"> </div>"
                    
-                newurl=newurl[delsize:len(newurl)-1]
+                newurl = newurl[delsize:len(newurl)-1]
                 print(typecontent.replace("$URL$",newurl))
     except Exception as er:
         print(er)
-    now=str(datetime.datetime.now().strftime("%d-%m-%Y %H:%M"))
+    now=  str(datetime.datetime.now().strftime("%d-%m-%Y %H:%M"))
     print("<footer>")
     print("<p>Правой нопкой мышки кликните на фотографию или видео и выберите пункт \"Сохранить изображение(видео) как...\"</p>")
     print("</footer>")
@@ -60,7 +60,7 @@ def htmlS():
     print('Content-type: text/html\n')
     with open("InstagramLo.html") as file:
         for i in range(0,25):
-            print(file.readline(),end="\n")
+            print(file.readline(), end="\n")
             
 htmlS()
 	
